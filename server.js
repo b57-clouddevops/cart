e017085ca5f32cfad2708643ce094219bcc33a18
CodingManoj <CodingManoj@gmail.com>
2024-04-02 18:10:15 -0400

    Moving Back To 100
    Revert "Updating cart to 300"
    
    This reverts commit b5cfb46a8819654d704107e1801760d28552c027.

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const pino = require('pino');
 const expPino = require('express-pino-logger');
-// Prometheus -Client -300 
+// Prometheus
 const promClient = require('prom-client');
 const Registry = promClient.Registry;
 const register = new Registry();
